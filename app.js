e691ed22e93a82e31b47a1e56529281c04dc5fda
mn39 <48751102+mn39@users.noreply.github.com>
2021-07-14 21:09:34 +0900

    feat : add emailjs

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 var nodes = null;
 var edges = null;
+var visnodes = null;
+var visedges = null;
+
 var network = null;
+var container = null;
+var options = null;
+var data = null;
 
-var netwk;
+
 function draw() {
-  // create people.
-  // value corresponds with the age of the person
 
-
-  fetch("https://lol-network-api.herokuapp.com")
+  fetch("https://lol-network-api.herokuapp.com/")
     .then((response) => response.json())
-    .then((data) => {
-      console.log(data)
-
+    .then((inpData) => {
+      console.log(inpData)
       nodes = []
-      var options = {
+      options = {
         layout: {
-          randomSeed: 0
+          // randomSeed: 0
         },
         nodes: {
-          borderWidth : 0,
+          borderWidth: 0,
           shape: "circularImage",
           image: 'https://opgg-static.akamaized.net/images/profile_icons/profileIcon1394.jpg?image=q_auto:best&v=1518361200',
           scaling: {
@@ -29,45 +31,107 @@ function draw() {
             min: 5,
             max: 150,
           },
-          font:{
-            size : 20,
-            border : 2,
+          font: {
+            size: 20,
           }
         },
-        edges:{
-          color : "#757575"
+        edges: {
+          color: "#757575"
         },
       };
-      
-      data.node.forEach(element => {
-        var temp = element
-        temp.label = temp.id
-        if(temp.label == '루모그래프'){
-          temp.shape = 'circularImage';
-          temp.image = 'https://opgg-static.akamaized.net/images/profile_icons/profileIcon4661.jpg?image=q_auto:best&v=1518361200';
+
+      for (var i = 0; i < inpData.node.length; i++) {
+        inpData.node[i].label = inpData.node[i].id;
+        if (inpData.node[i].label == '루모그래프') {
+          inpData.node[i].shape = 'circularImage';
+          inpData.node[i].image = 'https://opgg-static.akamaized.net/images/profile_icons/profileIcon4661.jpg?image=q_auto:best&v=1518361200';
         }
-        nodes.push(temp);
-
-      })
-
-
-      edges = data.edge
+      }
+      nodes = inpData.node
 
 
-      var container = document.getElementById("mynetwork");
-      var data = {
-        nodes: nodes,
-        edges: edges,
+      edges = inpData.edge
+
+
+      container = document.getElementById("mynetwork");
+
+      visnodes = new vis.DataSet(nodes);
+      visedges = new vis.DataSet(edges);
+      data = {
+        nodes: visnodes,
+        edges: visedges,
       };
+      console.log(edges)
       network = new vis.Network(container, data, options);
-      netwk = network;
+
+
+
     });
 }
 
-window.addEventListener("load", () => {
-  draw();
-  setTimeout(function() {
-    console.log(netwk);
+function remake() {
+  network = new vis.Network(container, data, options);
+}
 
-  }, 3000);
+function contactForm() {
+  document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // generate a five digit number for the contact_number variable
+    this.contact_number.value = Math.random() * 100000 | 0;
+    // these IDs from the previous steps
+    emailjs.sendForm('service_0gal12i', 'template_wqvlu05', this)
+      .then(function () {
+        console.log('SUCCESS!');
+      }, function (error) {
+        console.log('FAILED...', error);
+      });
+  });
+}
+window.addEventListener("load", () => {
+  contactForm()
+  draw();
+  setTimeout(function () {
+
+    edges.push({
+      from: "new",
+      value: 10,
+      to: "꿀벌지민"
+    })
+    edges.push({
+      from: "new",
+      value: 20,
+      to: "마리마리착마리"
+    })
+
+
+    edges.push({
+      from: "new2",
+      value: 10,
+      to: "new"
+    })
+    edges.push({
+      from: "new2",
+      value: 20,
+      to: "마리마리착마리"
+    })
+
+    edges.push({
+      from: "new2",
+      value: 30,
+      to: "리듬타지마"
+    })
+
+    nodes.push({
+      label: 'new',
+      id: 'new',
+    })
+    nodes.push({
+      label: 'new2',
+      id: 'new2',
+    })
+    visedges.update(edges)
+    visnodes.update(nodes)
+    // netwk.selectNodes([updatedIds[0]]);
+    // netwk.editNode();
+  }, 8000)
 });
