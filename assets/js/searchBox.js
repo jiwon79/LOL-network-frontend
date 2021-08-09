e00179630da24d6a8c271706b9c21c351fbf5fd9
mn39 <48751102+mn39@users.noreply.github.com>
2021-08-09 17:50:43 +0900

    feat : send depth by form

--- assets/js/searchBox.js
+++ assets/js/searchBox.js
@@ -1,10 +1,12 @@
-// updated 2019
-const input = document.getElementById("search-input");
-const searchBtn = document.getElementById("search-btn");
-
-const expand = () => {
-  searchBtn.classList.toggle("close");
-  input.classList.toggle("square");
-};
-
-searchBtn.addEventListener("click", expand);
+function searchToggle(obj, evt){
+  var container = $(obj).closest('.search-wrapper');
+      if(!container.hasClass('active')){
+          container.addClass('active');
+          evt.preventDefault();
+      }
+      else if(container.hasClass('active') && $(obj).closest('.input-holder').length == 0){
+          container.removeClass('active');
+          // clear input
+          container.find('.search-input').val('');
+      }
+}
