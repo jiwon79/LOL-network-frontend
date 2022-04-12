7c81fa61ab0deb1bf221b341a1358f0d90c5a301
jiwon79 <19-079@ksa.hs.kr>
2022-04-12 13:29:09 +0900

    fix: change emailjs setting

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,21 +1,22 @@
 function contactForm() {
-  document.getElementById('contact-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-    // generate a five digit number for the contact_number variable
-    this.contact_number.value = Math.random() * 100000 | 0;
-    // these IDs from the previous steps
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = (Math.random() * 100000) | 0;
+        // these IDs from the previous steps
 
-    emailjs.sendForm('service_0gal12i', 'template_wqvlu05', this)
-      .then(function () {
-        alert('피드백을 보냈습니다!');
-        document.getElementById('name').value = "";
-        document.getElementById('email').value = "";
-        document.getElementById('message').value = "";
-
-      }, function (error) {
-        console.log('FAILED...', error);
-      });
-  });
+        emailjs.sendForm('service_evbr12q', 'template_7l4z0kd', this).then(
+            function () {
+                alert('피드백을 보냈습니다!');
+                document.getElementById('name').value = '';
+                document.getElementById('email').value = '';
+                document.getElementById('message').value = '';
+            },
+            function (error) {
+                console.log('FAILED...', error);
+            }
+        );
+    });
 }
 
 contactForm();
