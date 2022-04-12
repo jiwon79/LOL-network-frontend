ac3f4ff87377b8284b14df0abe40de4f88968f3e
jiwon79 <19-079@ksa.hs.kr>
2022-04-12 14:25:08 +0900

    fix: fix emailjs send logic

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,5 +1,6 @@
 function contactForm() {
     document.getElementById('contact-form').addEventListener('submit', function (event) {
+        console.log('start');
         event.preventDefault();
         const templateParams = {
             contact_number: (Math.random() * 100000) | 0,
@@ -7,13 +8,15 @@ function contactForm() {
             email: document.getElementById('email').value,
             message: document.getElementById('message').value,
         };
+        console.log(templateParams);
 
-        emailjs.sendForm('service_evbr12q', 'template_7l4z0kd', templateParams).then(
+        emailjs.send('service_evbr12q', 'template_7l4z0kd', templateParams).then(
             function () {
                 alert('피드백을 보냈습니다!');
                 document.getElementById('name').value = '';
                 document.getElementById('email').value = '';
                 document.getElementById('message').value = '';
+                console.log('SUCCESS!', response.status, response.text);
             },
             function (error) {
                 console.log('FAILED...', error);
