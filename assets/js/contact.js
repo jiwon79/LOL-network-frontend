6eff1534e604e93cdde56dfa39fb67dee6032700
jiwon79 <19-079@ksa.hs.kr>
2022-04-12 13:39:08 +0900

    fix: fix send email logic

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,11 +1,14 @@
 function contactForm() {
     document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = (Math.random() * 100000) | 0;
-        // these IDs from the previous steps
+        const templateParams = {
+            contact_number: (Math.random() * 100000) | 0,
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            message: document.getElementById('message').value,
+        };
 
-        emailjs.sendForm('service_evbr12q', 'template_7l4z0kd', this).then(
+        emailjs.sendForm('service_evbr12q', 'template_7l4z0kd', templateParams).then(
             function () {
                 alert('피드백을 보냈습니다!');
                 document.getElementById('name').value = '';
